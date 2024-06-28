00ea6cb35b058ac54a268003eea17b7da57a4209
Webmania® <thyago@webmaniabr.com>
2024-06-28 09:12:01 -0300

    Update dfeToken.php

--- dfeToken.php
+++ dfeToken.php
@@ -1,14 +1,12 @@
 <?php 
 
 function createSecureTokenDFe( $password, $uuid ){
-
   $password = preg_replace("/[^0-9]/", '', $password);
   $key = hash('sha256', $password . ':' . $uuid, true);
   $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
   $encryptedData = openssl_encrypt(time(), 'AES-256-CBC', $key, OPENSSL_RAW_DATA, $iv);
   $tokenData = json_encode(['data' => base64_encode($encryptedData), 'iv' => base64_encode($iv)]);
   return urlencode(base64_encode($tokenData));
-
 }
 
 $password = '00.000.000/0000-00'; // CPF/CNPJ do tomador
